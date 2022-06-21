b83b73fde1cdae98fd359fc79acdf21a977d1321
Eddy Marcatoma Cardenas <eddyandres92@gmail.com>
2022-06-21 15:40:59 +0200

    Added Splash Screen
    Included dependency Lottie
    Included dependencies ViewModelScope, LifecycleScope, LiveData
    Create Android Resource Directory "raw" in "res" and included Lottie JSON
    Create SplashActivity, activity_splash
    Updated AndroidManifest

---
+++ app/src/main/java/com/example/personalhealth/SplashActivity.kt
@@ -0,0 +1,20 @@
+package com.example.personalhealth
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        lifecycleScope.launch {
+            delay(4000)
+            startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+            finish()
+        }
+    }
+}
